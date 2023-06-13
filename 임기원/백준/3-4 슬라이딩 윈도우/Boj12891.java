3d1965f31b46672f664fc0919273dfcb5f5e9c2e
IMGIWON <gwim1130@naver.com>
2023-06-13 15:33:03 +0900

    [BOJ]12891실버5

--- 임기원/백준/3-4 슬라이딩 윈도우/Boj12891.java
+++ 임기원/백준/3-4 슬라이딩 윈도우/Boj12891.java
@@ -4,12 +4,14 @@ package org.example;
 public class Boj12891 {
 import java.util .*;
 import java.io .*;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
 import java.util.Arrays;
     public class Main {
         static int checkArr[]; // 비밀번호 체크 배열
         static int myArr[]; // 비교한 현재 배열
         static int checkSecret; // 몇 개 충족했는지
-        public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException, NumberFormatException {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
             StringTokenizer st = new  StringTokenizer(bf.readLine());
             int S = Integer.parseInt(st.nextToken()); // 문자열 크기
@@ -19,7 +21,7 @@ import java.util.Arrays;
             checkArr = new int[4];
             myArr = new int[4];
             checkSecret = 0; // 충족개수 초기화
-            A = b.readLine().toCharArray();
+            A = bf.readLine().toCharArray();
             st = new StringTokenizer(bf.readLine());
             for (int i = 0; i < 4; i++) {
                 checkArr[i] = Integer.parseInt(st.nextToken());
@@ -30,16 +32,16 @@ import java.util.Arrays;
             for (int i = 0; i < P; i++) {
                 Add(A[i]);
             }
-            if(checkSecret == 4){
+            if(checkSecret == 4) {
                 count++;
-                // 슬라이딩 윈도우 처리 부분
-                for (int i = P; i < S; i++) {
-                    int j = i - _;
-                    Add(A[i]);
-                    Remove(A[j]);
-                    if(checkSecret == 4){
-                        count++;
-                    }
+            }
+            // 슬라이딩 윈도우 처리 부분
+            for (int i = P; i < S; i++) {
+                int j = i - P;
+                Add(A[i]);
+                Remove(A[j]);
+                if(checkSecret == 4){
+                    count++;
                 }
             }
             System.out.println(count);
@@ -50,25 +52,25 @@ import java.util.Arrays;
             switch (c) {
                 case 'A':
                     myArr[0]++;
-                    if(myArr[0] == checkSecret[0]){
+                    if(myArr[0] == checkArr[0]){
                         checkSecret++;
                     }
                     break;
                 case 'C':
                     myArr[1]++;
-                    if(myArr[1] == checkSecret[1]){
+                    if(myArr[1] == checkArr[1]){
                         checkSecret++;
                     }
                     break;
                 case 'G':
                     myArr[2]++;
-                    if(myArr[2] == checkSecret[2]){
+                    if(myArr[2] == checkArr[2]){
                         checkSecret++;
                     }
                     break;
                 case 'T':
                     myArr[3]++;
-                    if(myArr[3] == checkSecret[3]){
+                    if(myArr[3] == checkArr[3]){
                         checkSecret++;
                     }
                     break;
@@ -78,28 +80,28 @@ import java.util.Arrays;
         private static void Remove(char c) {
             switch (c) {
                 case 'A':
-                    myArr[0]--;
-                    if (myArr[0] == checkSecret[0]) {
+                    if (myArr[0] == checkArr[0]) {
                         checkSecret--;
                     }
+                    myArr[0]--;
                     break;
                 case 'C':
-                    myArr[1]--;
-                    if (myArr[1] == checkSecret[1]) {
+                    if (myArr[1] == checkArr[1]) {
                         checkSecret--;
                     }
+                    myArr[1]--;
                     break;
                 case 'G':
-                    myArr[2]--;
-                    if (myArr[2] == checkSecret[2]) {
+                    if (myArr[2] == checkArr[2]) {
                         checkSecret--;
                     }
+                    myArr[2]--;
                     break;
                 case 'T':
-                    myArr[3]--;
-                    if (myArr[3] == checkSecret[3]) {
+                    if (myArr[3] == checkArr[3]) {
                         checkSecret--;
                     }
+                    myArr[3]--;
                     break;
             }
         }
