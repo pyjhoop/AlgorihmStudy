d41a50e3955e0f897a63cb7fb250017561f7e8a7
pyjhoop <pyjhoop@naver.com>
2023-06-20 11:43:48 +0900

    [PGS] 타겟넘버/ LEVEL2

---
+++ 박연준/코드리뷰 문제/타겟넘버.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class 타겟넘버 {
+    int count = 0;
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        dfs(numbers,0,target,0);
+        answer = count;
+        return answer;
+    }
+
+    public void dfs(int[] numbers, int depth, int target, int result){
+
+        if(depth == numbers.length){ // 깊이가 노드의 끝에 도착할 경우
+            if(target == result){
+                count++;
+            }
+            return;
+        }
+
+        int plus = result + numbers[depth];
+        int minus = result - numbers[depth];
+
+        dfs(numbers, depth+1, target, plus);
+        dfs(numbers, depth+1, target, minus);
+    }
+}
